81754ed98e41dde398620a3616af27e03c1ddb14
Jahnavi41 <astrantia236@gmail.com>
2025-07-09 17:52:22 +0530

    DI-field, constructor, setter and method injection

--- First/src/main/java/com/example/First/MyFirstService.java
+++ First/src/main/java/com/example/First/MyFirstService.java
@@ -2,16 +2,38 @@ package com.example.First;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-    @Autowired
-    @Qualifier("bean2")
     private MyFirstClass myFirstClass;
+    private Environment environment;
 
+    @Autowired
+     void setMyFirstClass(@Qualifier("bean2")MyFirstClass myFirstClass) {
+        this.myFirstClass = myFirstClass;
+    }
+
+    @Autowired
+    void setEnvironment(Environment environment){
+        this.environment = environment;
+    }
+
+    public String getJavaVersion() {
+        return "Java version is: "+environment.getProperty("java.version");
+    }
+
+    public String getOs() {
+        return "OS is: "+environment.getProperty("os.name");
+    }
     public String myFirstService() {
         return "The dependency is saying: "+myFirstClass.sayHello();
     }
 }
+
+
+//constructor, method, setter and field injection
+//Can use @Autowired if more than one var's are being injected
+//We have to use Qualifier or primary annotations in case there are multiple beans
